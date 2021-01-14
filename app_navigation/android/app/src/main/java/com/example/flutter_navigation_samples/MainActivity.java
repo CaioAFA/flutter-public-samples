e2c72a0eff77207d6498ee21273b5d6d749172d9
Caio Arrabal <caio.a.1998@gmail.com>
2021-01-13 22:04:15 -0300

    First Commit.

---
+++ app_navigation/android/app/src/main/java/com/example/flutter_navigation_samples/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.flutter_navigation_samples;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ app_navigation/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_navigation_samples/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: HomeScreen(),
+    );
+  }
+}
